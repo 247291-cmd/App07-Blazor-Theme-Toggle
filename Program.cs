3fc009427084b000f06cd1ffed91a3a47709b21b
247291-cmd <247291cmd@student.com>
2026-05-23 15:43:58 +0500

    Initial commit: App07-Blazor-Theme-Toggle project setup

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using App07_ThemeToggle.Components;
+using App07_ThemeToggle.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRazorComponents().AddInteractiveServerComponents();
+builder.Services.AddSingleton<ThemeService>();
+
+var app = builder.Build();
+if (!app.Environment.IsDevelopment()) { app.UseExceptionHandler("/Error", createScopeForErrors: true); app.UseHsts(); }
+app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+app.UseHttpsRedirection();
+app.UseAntiforgery();
+app.MapStaticAssets();
+app.MapRazorComponents<App>().AddInteractiveServerRenderMode();
+app.Run();
---
+++ Services/ThemeService.cs
@@ -0,0 +1,11 @@
+﻿namespace App07_ThemeToggle.Services;
+
+public class ThemeService
+{
+    public string CurrentTheme { get; private set; } = "light";
+    public bool IsDarkMode => CurrentTheme == "dark";
+    public event Action? OnThemeChanged;
+
+    public void SetTheme(string theme) { CurrentTheme = theme == "dark" ? "dark" : "light"; OnThemeChanged?.Invoke(); }
+    public void Toggle() => SetTheme(IsDarkMode ? "light" : "dark");
+}
